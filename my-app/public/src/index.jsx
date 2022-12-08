4c4d4caf23e9e9eb2b7d92467dfd52359cfd8c44
Lorenzobenitez87 <105927927+Lorenzobenitez87@users.noreply.github.com>
2022-12-08 10:24:14 +0100

    excercice shiny

--- my-app/public/src/index.jsx
+++ my-app/public/src/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import Home from './pages/Home';
+import Home from './pages/Home/index';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+ 
+ReactDOM.render(
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
